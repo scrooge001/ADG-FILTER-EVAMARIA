f783ad0de059b583ddc7926f7f5597a62c33979a
krk <72791356+scrooge001@users.noreply.github.com>
2022-12-16 12:10:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -63,8 +63,8 @@ async def next_page(bot, query):
     if settings['button']:
         btn = [
             [
-                InlineKeyboardButton(💾
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                InlineKeyboardButton("🌟",
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", "🗒",callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
